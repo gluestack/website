900d78c51eaad89cfa8a5a8bbdef7b34494a9ae4
vidhi499 <katariavidhi99@gmail.com>
2022-12-09 15:19:29 +0530

    build fixes

--- components/FirstFold.tsx
+++ components/FirstFold.tsx
@@ -29,7 +29,6 @@ export const FirstFold = () => {
               <a
                 className="group relative inline-flex border border-red-600 focus:outline-none w-full sm:w-auto"
                 href="https://laracasts.com"
-                target="_blank"
               >
                 <span className="w-full inline-flex items-center justify-center self-stretch px-4 py-2 text-sm text-red-600 text-center font-bold uppercase bg-white ring-1 ring-red-600 ring-offset-1 transform transition-transform group-hover:-translate-y-1 group-hover:-translate-x-1 group-focus:-translate-y-1 group-focus:-translate-x-1">
                   Watch Laracasts
