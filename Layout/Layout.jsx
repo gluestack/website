b6ceaa71bc7efe50397cf5ef4ac54057b0002a0c
shrutika <shrutika@geekyants.com>
2022-12-12 12:49:21 +0530

    merge branch

--- Layout/Layout.jsx
+++ Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Footer } from "../components/Footer";
+import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
 function Layout({ children }) {
@@ -7,7 +7,7 @@ function Layout({ children }) {
     <>
       <Header />
       {children}
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
 /** @type {import('next').NextConfig} */
-const { DOCS_URL } = process.env;
 const nextConfig = {
-  // reactStrictMode: true,
-  // swcMinify: true,
-  // assetPrefix: `${DOCS_URL}`,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: `/:path*`,
-      },
-      {
-        source: "/docs",
-        destination: `${DOCS_URL}/docs`,
-      },
-      {
-        source: "/docs/:path*",
-        destination: `${DOCS_URL}/docs/:path*`,
-      },
-    ];
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ["localhost"] },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
