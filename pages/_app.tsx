74503abb17d0adfcef928a88a55119d6876065b5
vidhi499 <katariavidhi99@gmail.com>
2022-12-13 22:09:33 +0530

    fix: navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,37 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-// @ts-ignore
-import { Layout } from "@gluestack/docs-layout";
+import Layout from "../Layout/Layout";
 import { useState } from "react";
 import { versions } from "../versions.json";
+import PagesLayout from "../Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log(pageProps, "PAGE PROPS");
+  // console.log(pageProps, "PAGE PROPS");
   const router = useRouter();
-  console.log(router.pathname);
-  const [version, setVersion]: any = useState(
-    Object.keys(versions[versions.length - 1])
-  );
+  // console.log(router.pathname);
+  // const [version, setVersion]: any = useState(
+  //   Object.keys(versions[versions.length - 1])
+  // );
 
-  function getSidebarJsonData() {
-    for (let i = 0; i < versions.length; i++) {
-      if (Object.keys(versions[i])[0] == version) {
-        return versions[i];
-      }
-    }
-  }
+  // function getSidebarJsonData() {
+  //   for (let i = 0; i < versions.length; i++) {
+  //     if (Object.keys(versions[i])[0] == version) {
+  //       return versions[i];
+  //     }
+  //   }
+  // }
   if (router.pathname.includes("/docs")) {
     return (
+      <PagesLayout>
+        <Component {...pageProps} />
+      </PagesLayout>
       // <Layout
       // // version={version}
       // // versionInfo={getSidebarJsonData()}
       // // setVersion={setVersion}
       // // versionsData={versions}
       // >
-      <Component {...pageProps} />
       // </Layout>
     );
   } else {
