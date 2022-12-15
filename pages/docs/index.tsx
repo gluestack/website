ac5f0dffe5b71a665696af34fbdfdabeb3be9952
vidhi499 <katariavidhi99@gmail.com>
2022-12-15 12:15:20 +0530

    fixes

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,31 +1,10 @@
-import React from "react";
-// @ts-ignore
-import { Layout } from "@gluestack/docs-layout";
-import { versions } from "../../versions.json";
-import { useState } from "react";
+export default function () {}
 
-export const Docs = () => {
-  // const [version, setVersion]: any = useState(
-  //   Object.keys(versions[versions.length - 1])
-  // );
-
-  // function getSidebarJsonData() {
-  //   for (let i = 0; i < versions.length; i++) {
-  //     if (Object.keys(versions[i])[0] == version) {
-  //       return versions[i];
-  //     }
-  //   }
-  // }
-  // console.log("Loaded", version);
-  return (
-    // <Layout
-    //   version={version}
-    //   versionInfo={getSidebarJsonData()}
-    //   setVersion={setVersion}
-    //   versionsData={versions}
-    // >
-    <div>hihihi in docjknkjnkjs</div>
-    // </Layout>
-  );
-};
-export default Docs;
+// export async function getServerSideProps(context: any) {
+//   return {
+//     redirect: {
+//       permanent: false,
+//       destination: context.resolvedUrl + "/" + indexSlugFileName,
+//     },
+//   };
+// }
--- scripts/get-latest-version-data.js
+++ scripts/get-latest-version-data.js
@@ -8,26 +8,60 @@ const fs = require("fs-extra");
 const pagesPath = path.join(__dirname, "../pages");
 const ignoredfolder = ["_app.tsx", "index.tsx", "api"];
 const tmpPath = path.join(__dirname, "tmp");
-
+let data1 = [];
 const cloneRepoPages = () => {
   Object.keys(repoConfig).map((e) => {
     // console.log(e, repoConfig[e]);
     let repoInfo = repoConfig[e];
+
     let copyPath = path.join(pagesPath, repoInfo.destinationPath);
-    execSync("git clone " + repoConfig[e].gitUrl, {
-      stdio: [0, 1, 2], // we need this so node will print the command output
-      cwd: tmpPath, // path to where you want to save the file
-    });
-    if (!fs.existsSync(path.join(copyPath))) {
-      fs.mkdirSync(path.join(copyPath));
+    // execSync("git clone " + repoConfig[e].gitUrl, {
+    //   stdio: [0, 1, 2], // we need this so node will print the command output
+    //   cwd: tmpPath, // path to where you want to save the file
+    // });
+    if (
+      fs.existsSync(path.join(tmpPath, repoInfo["repoName"], "versions.json"))
+    ) {
+      let data = fs.readFileSync(
+        path.join(tmpPath, repoInfo["repoName"], "versions.json"),
+        {
+          encoding: "utf-8",
+        }
+      );
+      data = JSON.parse(data);
+      data1 = [
+        ...data1,
+        getLatestVersion(data.versions)[
+          Object.keys(getLatestVersion(data.versions))[0]
+        ].sidebar,
+      ];
+      console.log(
+        // getLatestVersion(data.versions)[
+        //   Object.keys(getLatestVersion(data.versions))[0]
+        // ].sidebar,
+        data1,
+        JSON.stringify(data1)
+      );
+      // if (!fs.existsSync(path.join(copyPath))) {
+      //   fs.mkdirSync(path.join(copyPath));
+      // }
+      // copyFiles(
+      //   path.join(
+      //     tmpPath,
+      //     repoInfo["repoName"],
+      //     "pages",
+      //     Object.keys(getLatestVersion(data.versions))[0]
+      //   ),
+      //   path.join(copyPath)
+      // );
+      // deleteIgnoredFiles(path.join(copyPath));
     }
-    copyFiles(
-      path.join(tmpPath, repoInfo["repoName"], repoInfo["pagesPath"]),
-      path.join(copyPath)
-    );
-    // deleteIgnoredFiles(path.join(copyPath));
   });
 };
+
+function getLatestVersion(data) {
+  return data[data.length - 1];
+}
 
 const deleteIgnoredFiles = (path1) => {
   for (let i = 0; i < ignoredfolder.length; i++) {
