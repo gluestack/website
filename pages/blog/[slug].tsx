3cf2463255e9fb4bb0f3b1c1a50b94123276e5e9
nishant7156 <shedagenishant40@gmail.com>
2022-12-16 15:59:32 +0530

    blog(working)

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div>[slug]</div>
+  )
+}
+
+export default Blog
---
+++ pages/blog/index.tsx
@@ -0,0 +1,14 @@
+
+import Blogs from '../../section/Blog/Blogs'
+import HeroSection from "../../section/Blog/HeroSection";
+
+function Blog() {
+  return (
+    <>
+    <HeroSection/>
+     <Blogs/>
+    </>
+  );
+}
+
+export default Blog;
