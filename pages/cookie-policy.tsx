41ac6cf1d252b965ebfd33a4ffa75ae0df753a75
shrutika <shrutika@geekyants.com>
2022-12-17 18:38:10 +0530

    added cookies

---
+++ pages/cookie-policy.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Details from '../section/Terms/Details'
+import HeroSection from '../section/Terms/HeroSection'
+import Layout from "../Layout/Layout";
+
+function Cookies() {
+  return (
+    <Layout>
+        <HeroSection/>
+        <Details/>
+    </Layout>
+  )
+}
+
+export default Cookies;
