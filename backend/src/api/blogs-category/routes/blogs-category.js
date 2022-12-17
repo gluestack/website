e7b7107e4e082afb984cd4024033244bd26e55c5
nishant7156 <shedagenishant40@gmail.com>
2022-12-17 12:32:21 +0530

    setupstrapi for blog page

---
+++ backend/src/api/blogs-category/routes/blogs-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blogs-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blogs-category.blogs-category');
