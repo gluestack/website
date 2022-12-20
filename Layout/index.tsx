2b63a9889c5e533f3190b25b38c0e521c4e4c856
Saudamini <saudamini@geekyants.com>
2022-12-20 17:24:49 +0530

    Added responsiveness

--- Layout/index.tsx
+++ Layout/index.tsx
@@ -8,9 +8,15 @@ import SidebarLatest from "./components/SidebarLatest";
 import ContentHeader from "./components/ContentHeader";
 import { useRouter } from "next/router";
 import { IFrameContent } from "./components/IFrameContent";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
+import Logo from "../components/Header/Logo";
 
 function PagesLayout(props: any) {
   const router = useRouter();
+  const [sideBarOpen, setSideBar] = useState(false);
+  const hamburgerClick = () => setSideBar(!sideBarOpen);
+
   console.log(router.route);
   return (
     <div className={"flex flex-1 min-h-screen relative layout-theme " + (router.route == '/docs/1.x/gluestack-ui' ? 'fixed' : '')}>
@@ -21,8 +27,18 @@ function PagesLayout(props: any) {
         versionsData={props.versionsData}
       /> */}
       {/* <Header /> */}
+      <div className={"md:hidden mobile-header flex flex-row justify-between" + (sideBarOpen ? ' shadow-md': '')}>
+          <div className="w-[110px]"><Logo /></div>
+          <div onClick={hamburgerClick} className="ease-in-out transition-2">
+              {sideBarOpen ? <AiOutlineClose/> : <GiHamburgerMenu/>}
+          </div>
+         
+      </div>
       <div className="flex w-full min-h-full full-doc-content">
-        <SidebarLatest versions={versions} />
+        <div className={"sidebarNav " + (sideBarOpen ? 'showNav': 'hideNav') + ' left-'+(sideBarOpen ? '[-100%]' : '0')} >
+          <SidebarLatest versions={versions} />
+        </div>
+        
         {/* <Sidebar
           versions={versions}
           // {...props.versionInfo[props.version]}
@@ -34,7 +50,7 @@ function PagesLayout(props: any) {
         ) : (
           <div className="content-holder flex flex-1 flex-col">
             <ContentHeader />
-            <div className="flex-1 h-full prose lg:prose-xl">
+            <div className="content flex-1 h-full prose lg:prose-xl">
               <Content {...props} />
             </div>
           </div>
