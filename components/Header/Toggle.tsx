f4502baf2d1945dbefaa9eaf663be60aca3d7394
Megha Kumari <meghakumari@Megha-Kumari-B103-MacBook-Pro-16.local>
2022-12-20 18:51:25 +0530

    removed toggle theme

--- components/Header/Toggle.tsx
+++ components/Header/Toggle.tsx
@@ -6,7 +6,7 @@ function Toggle() {
 
   return (
     <>
-      {colorTheme === "light" ? (
+      {/* {colorTheme === "light" ? (
         <svg
           onClick={() => setTheme("light")}
           xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +38,7 @@ function Toggle() {
             d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
           />
         </svg>
-      )}
+      )} */}
     </>
   );
 }
