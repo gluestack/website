fedcf49e25b6e266a9e7ecc39fde41f74e31fe5d
shrutika <shrutika@geekyants.com>
2022-12-21 21:50:00 +0530

    fix footer

--- section/Home/SignupFooter.tsx
+++ section/Home/SignupFooter.tsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 import logodark from "/public/images/logo/darkLogo.svg";
 import line from "/public/images/line.png";
 import CustomImage from "../../components/CustomImage";
+import Text from "../../elements/Text";
 
 function SignupFooter() {
   return (
     <div className="relative w-full bg-black py-16 ">
         <div className="container items-center justify-center flex flex-col flex-1">
-=            <div className="w-[300px] h-[150px] text-center">
+          <div className="w-[300px]  text-center">
                 <CustomImage src={logodark} alt="image" />
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row items-center mt-2">
+            <div className="w-[100px]  text-center">
                 <CustomImage src={line} alt="image" />
-                <p className="text-white">Now build scalable full-stack apps faster</p>
+                </div>
+                <Text style="text-white ml-2" size="sm">Now build scalable full-stack apps faster</Text>
             </div>
         </div>
     </div>
