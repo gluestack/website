5a7cd2885bac34d62826d4d2b7e625d055e7ef27
cyrus25 <cyruspassi@gmail.com>
2022-12-21 17:57:19 +0530

    Added cookie policy

--- components/Cookies.tsx
+++ components/Cookies.tsx
@@ -1,58 +1,110 @@
-import CookieConsent from "react-cookie-consent";
-import Link from "next/link";
+import { useEffect } from "react";
 
-function Cookies() {
-  return (
-    <CookieConsent
-      containerClasses="cookies"
-      style={{
-        background: "rgba(25, 25, 25, 0.9)",
-        fontSize: "12px",
-        color: "#efefef",
-        padding: "0px 10px",
-        lineHeight: "1.2em",
-        textAlign: "center",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: "9999999",
-      }}
-      contentClasses="container"
-      buttonStyle={{
-        backgroundColor: "#8944CE",
-        position: "relative",
-        color: "#fff !important",
-        fontSize: "12px",
-        padding: "5px 8px",
-        width: "auto",
-        minWidth: "auto",
-        zIndex: "99",
-      }}
-      buttonText="Accept"
-    >
-      <div className="flex lg:flex-row flex-col items-center lg:text-xs text-xxs">
-        We use our own and third-party cookies and other tracking technologies,
-        by continuing to browse the website, you accept our use of cookies and
-        tracking technologies
-        <div
-          className="lg:ml-2 ml-0 lg:mt-0 mt-2 hover:underline relative"
-          style={{ padding: "5px 8px", boxShadow: "inset 0 0 2px #8944CE" }}
-        >
-          <Link
-            href="/cookie-policy"
-            className="	"
-            style={{ boxShadow: "inset 0 0 2px #8944CE" }}
-            prefetch={false}
-            legacyBehavior
-          >
-            <a className="ml-auto font-normal text-white-100 text-xs shadow-sm whitespace-nowrap">
-              Learn more
-            </a>
-          </Link>
-        </div>
-      </div>
-    </CookieConsent>
-  );
+import "vanilla-cookieconsent";
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+
+export default function Cookies() {
+  useEffect(() => {
+    if (window && !document.getElementById("cc--main")) {
+      // @ts-ignore
+      window.CookieConsentApi = window.initCookieConsent();
+      // @ts-ignore
+      window.CookieConsentApi.run({
+        current_lang: "en",
+        autoclear_cookies: true, // default: false
+        page_scripts: true, // default: false
+
+        languages: {
+          en: {
+            consent_modal: {
+              title: "We use cookies!",
+              description:
+                'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
+              primary_btn: {
+                text: "Accept all",
+                role: "accept_all", // 'accept_selected' or 'accept_all'
+              },
+              secondary_btn: {
+                text: "Reject all",
+                role: "accept_necessary", // 'settings' or 'accept_necessary'
+              },
+            },
+            settings_modal: {
+              title: "Cookie preferences",
+              save_settings_btn: "Save settings",
+              accept_all_btn: "Accept all",
+              reject_all_btn: "Reject all",
+              close_btn_label: "Close",
+              cookie_table_headers: [
+                { col1: "Name" },
+                { col2: "Domain" },
+                { col3: "Expiration" },
+                { col4: "Description" },
+              ],
+              blocks: [
+                {
+                  title: "Cookie usage 📢",
+                  description:
+                    'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.',
+                },
+                {
+                  title: "Strictly necessary cookies",
+                  description:
+                    "These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly",
+                  toggle: {
+                    value: "necessary",
+                    enabled: true,
+                    readonly: true, // cookie categories with readonly=true are all treated as "necessary cookies"
+                  },
+                },
+                {
+                  title: "Performance and Analytics cookies",
+                  description:
+                    "These cookies allow the website to remember the choices you have made in the past",
+                  toggle: {
+                    value: "analytics", // your cookie category
+                    enabled: false,
+                    readonly: false,
+                  },
+                  cookie_table: [
+                    // list of all expected cookies
+                    {
+                      col1: "^_ga", // match all cookies starting with "_ga"
+                      col2: "google.com",
+                      col3: "2 years",
+                      col4: "description ...",
+                      is_regex: true,
+                    },
+                    {
+                      col1: "_gid",
+                      col2: "google.com",
+                      col3: "1 day",
+                      col4: "description ...",
+                    },
+                  ],
+                },
+                {
+                  title: "Advertisement and Targeting cookies",
+                  description:
+                    "These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you",
+                  toggle: {
+                    value: "targeting",
+                    enabled: false,
+                    readonly: false,
+                  },
+                },
+                {
+                  title: "More information",
+                  description:
+                    'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
+                },
+              ],
+            },
+          },
+        },
+      });
+    }
+  }, []);
+
+  return null;
 }
-
-export default Cookies;
