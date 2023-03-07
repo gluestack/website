94954bc5916154052c6d5e02a2b82ae1a2486614
Faiz A. Farooqui <faizz.af@gmail.com>
2023-03-07 16:35:12 +0530

    chore: added deploy & web plugin docs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,6 +29,8 @@ import storybookPluginSidebarData from "./docs/plugins/storybook/versions.json";
 import serviceNodePluginSidebarData from "./docs/plugins/service-node/versions.json";
 import serviceNodemailerPluginSidebarData from "./docs/plugins/service-nodemailer/versions.json";
 import pgAdminPluginSidebarData from "./docs/plugins/pg-admin/versions.json";
+import deployPluginSidebarData from "./docs/plugins/deploy/versions.json";
+import webPluginSidebarData from "./docs/plugins/web/versions.json";
 
 export const AppContext = React.createContext({} as unknown as any);
 
@@ -50,6 +52,8 @@ function MyApp({ children }: { children: React.ReactNode }) {
     "glue-plugin-service-node": serviceNodePluginSidebarData,
     "glue-plugin-service-nodemailer": serviceNodemailerPluginSidebarData,
     "glue-plugin-pg-admin": pgAdminPluginSidebarData,
+    "glue-plugin-deploy": deployPluginSidebarData,
+    "glue-plugin-web": webPluginSidebarData,
   };
 
   let repos = storybookToNextDocs.repos;
